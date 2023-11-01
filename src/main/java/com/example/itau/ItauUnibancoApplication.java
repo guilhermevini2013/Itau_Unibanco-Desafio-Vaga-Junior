46519f8a4b53796196e831508bf71b9554adb191
01nevida <01@vida>
2023-11-01 20:12:28 -0300

    Initial commit

---
+++ src/main/java/com/example/itau/ItauUnibancoApplication.java
@@ -0,0 +1,13 @@
+package com.example.itau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauUnibancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauUnibancoApplication.class, args);
+	}
+
+}
